a179d33b25042d149c4e67c4c1d8bfcce86301d2
ssgamingop <somyajeetsingh15@gmail.com>
2026-05-15 20:56:53 +0530

    feat: add X icon to AlertsWidget component imports

--- src/components/AlertsWidget.tsx
+++ src/components/AlertsWidget.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useAlertsStore } from '../store/useAlertsStore';
 import { cryptoApi } from '../services/api';
 import toast from 'react-hot-toast';
-import { Bell, BellRing, Plus, Trash2 } from 'lucide-react';
+import { Bell, BellRing, Plus, Trash2, X } from 'lucide-react';
 
 export const AlertsWidget: React.FC = () => {
   const { alerts, addAlert, removeAlert, toggleAlertActive } = useAlertsStore();
